0f76f3db517e2caa75e2e0595b742eb4f2f74139
v1b3m <vibenjamin6@gmail.com>
2025-04-21 13:58:57 +0300

    Keep Hacking

---
+++ SWEBench Review Phase 2.1.4/django__django-11734/incorrect/before_query.py
@@ -0,0 +1,8 @@
+        handle.
+        """
+        filter_lhs, filter_rhs = filter_expr
+        if isinstance(filter_rhs, F):
+            filter_expr = (filter_lhs, OuterRef(filter_rhs.name))
+        # Generate the inner query.
+        query = Query(self.model)
+        query._filtered_relations = self._filtered_relations
